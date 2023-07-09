eeeeeb7cb8fe574ff5ec36a98c7cf3f7b45e0bba
Camryn Scott <camryn1242@yahoo.com>
2023-07-09 19:45:15 -0400

    finished Cupcake model and added seed file

--- models.py
+++ models.py
@@ -5,3 +5,23 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
+DEFAULT_IMG = 'https://tinyurl.com/demo-cupcake'
+
+
+class Cupcake(db.Model):
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.Text,
+                       nullable=False)
+    size = db.Column(db.Text,
+                     nullable=False)
+    rating = db.Column(db.Float,
+                       nullable=False)
+    image = db.Column(db.Text,
+                      nullable=False,
+                      default=DEFAULT_IMG)
